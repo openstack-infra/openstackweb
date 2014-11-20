43703c7b5a2beb089c8a0e997ead64604a636da7
Sebastian Marcet <smarcet@gmail.com>
2014-11-20 15:53:09 -0300

    [smarcet] * migration fixes for ss3.

--- registration/_config.php
+++ registration/_config.php
@@ -14,7 +14,3 @@
 Object::add_extension('Member', 'MemberDecorator');
 Object::add_extension('SecurityAdmin', 'SecurityAdminDecorator');
 Object::add_extension('SiteConfig', 'CustomSiteConfigRegistration');
-
-Director::addRules(100, array(
-    'userprofile' => 'AffiliationController'
-));
